97060000e69c255c131d8f10f938af22698ea21a
Ferret-Dev <89497890+Ferret-Dev@users.noreply.github.com>
2023-01-07 12:31:43 +1200

    Upload main.py
    Uploading original files

---
+++ main.py
@@ -0,0 +1,45 @@
+#ALL IMPORTS
+import os
+# filesystem
+import logging
+# for error handling
+import discord
+# discord api wrapper
+import live
+# keep_alive file, only for replit
+from dotenv import load_dotenv
+# client secrets + keys
+from discord.ext import commands
+# discord bot client with commands
+import asyncio
+# library for doing time-based stuff
+
+#DEFINITIONS + SETUP
+load_dotenv()
+# loads keys
+TOKEN = os.getenv('D_TOKEN')
+# grabs discord token from .env
+intents = discord.Intents.all()
+# specifies intents used by Lavender
+bot = commands.Bot(command_prefix="!-", help_command=None, intents=intents)
+# makes bot commands and enables all intents
+
+#RUN BOT
+live.live()
+# runs keep_alive ('live') file, only for replit
+discord.utils.setup_logging()
+# for handling logs, I don't have the energy to fully grasp this right now
+
+async def load_extensions():
+  for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+    # cut off the .py from the file name
+      await bot.load_extension(f"cogs.{filename[:-3]}")
+      # loads all cogs in './cogs' directory
+
+async def main():
+  async with bot:
+    await load_extensions()
+    await bot.start(TOKEN)
+asyncio.run(main())
+# runs bot, obviously
